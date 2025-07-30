7b5429f82e3cdc61b5166db0786b657450cc10d9
umar mohammad sheikh <sumar@proficientnowtech.com>
2025-07-30 18:01:09 +0530

    resolve test

--- packages/zoo/src/lib/zoo.ts
+++ packages/zoo/src/lib/zoo.ts
@@ -1,8 +1,11 @@
-import { getRandomAnimal } from '@acme/animal';
-import { formatMessage } from '@acme/util';
+// import { getRandomAnimal } from '@acme/animal';
+// import { formatMessage } from '@acme/util';
 
+// export function zoo(): string {
+//   const result = getRandomAnimal();
+//   const message = `${result.name} says ${result.sound}!`;
+//   return formatMessage('ZOO', message);
+// }
 export function zoo(): string {
-  const result = getRandomAnimal();
-  const message = `${result.name} says ${result.sound}!`;
-  return formatMessage('ZOO', message);
+  return 'zoo';
 }
